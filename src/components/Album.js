30df097c1086dfafcdcbd33b5ec6439b127a8eb1
teddylopez <tedmlopez@gmail.com>
2018-01-21 18:08:37 -0500

    Created player-bar functionality.

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 
 class Album extends Component {
@@ -39,9 +40,30 @@ class Album extends Component {
      if (this.state.isPlaying && isSameSong) {
        this.pause();
      } else {
-       if (!isSameSong) { this.setSong(song); } 
+       if (!isSameSong) { this.setSong(song); }
        this.play();
      }
+   }
+
+   handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = currentIndex + 1
+
+    if (newIndex < this.state.album.songs.length) {
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play(newSong);
+    } else {
+      this.play(this.state.currentSong);
+    }
+  }
+
+   handlePrevClick() {
+     const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+     const newIndex = Math.max(0, currentIndex - 1);
+     const newSong = this.state.album.songs[newIndex];
+     this.setSong(newSong);
+     this.play(newSong);
    }
 
   render() {
@@ -73,6 +95,13 @@ class Album extends Component {
           }
            </tbody>
          </table>
+         <PlayerBar
+           isPlaying={this.state.isPlaying}
+           currentSong={this.state.currentSong}
+           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+           handlePrevClick={() => this.handlePrevClick()}
+           handleNextClick={() => this.handleNextClick()}
+         />
       </section>
     );
   }

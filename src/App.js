dafd626371e758eb15c32e8e2607acde1e5cdb94
teddylopez <tedmlopez@gmail.com>
2018-01-26 12:35:08 -0500

    Modified landing page stlyes.

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ class App extends Component {
     return (
       <div className="App">
         <header>
-          <nav>
-            <Link to='/'>Landing</Link>
-            <Link to='/library'>Library</Link>
-          </nav>
-          <h1>Bloc Jams</h1>
+          <Link to='/' id="logo">TedTunes</Link>
+            <nav className="clearfix">
+              <Link to='/'>Landing</Link>
+              <Link to='/library'>Library</Link>
+            </nav>
         </header>
         <main>
           <Route exact path="/" component={Landing} />
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,18 +2,25 @@ import React from 'react';
 
 const Landing = () => (
   <section className="landing">
-    <h1 className="hero-title">Turn the music up!</h1>
+  <section className="hero-content">
+    <div className="hero-title">
+      <h1>Turn It Up</h1>
+    </div>
+  </section>
 
-    <section className="selling-points">
+    <section className="selling-points clearfix">
       <div className="point">
+        <span className="ion-music-note"></span>
         <h2 className="point-title">Choose your music</h2>
-        <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
+        <p className="point-description">The world is full of music; why should you have to listen to anything that Ted didnt write?</p>
       </div>
       <div className="point">
+        <span className="ion-radio-waves"></span>
         <h2 className="point-title">Unlimited, streaming, ad-free</h2>
         <p className="point-description">No arbitrary limits. No distractions.</p>
       </div>
       <div className="point">
+        <span className="ion-iphone"></span>
         <h2 className="point-title">Mobile enabled</h2>
         <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
       </div>

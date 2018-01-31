a34ef48952a3f9139dbe6b297e2fd8ced74d79ce
teddylopez <tedmlopez@gmail.com>
2018-01-31 18:20:56 -0500

    Added more albums.

--- src/data/albums.js
+++ src/data/albums.js
@@ -18,7 +18,7 @@ export default [{
     albumCover: '/assets/images/album_covers/Album2.png',
     slug: 'Welcome',
     songs: [
-      { title: 'Blue', duration: '161.71', playCount: 0, audioSrc: '/assets/music/blue.mp3' },
+      { title: 'Blue', duration: '161.71', playCount: 0, audioSrc: '/assets/music/song-1.mp3' },
       { title: 'Green', duration: '103.96', playCount: 0, audioSrc: '/assets/music/green.mp3' },
       { title: 'Red', duration: '268.45', playCount: 0, audioSrc: '/assets/music/red.mp3' },
       { title: 'Pink', duration: '153.14', playCount: 0, audioSrc: '/assets/music/pink.mp3' },
@@ -31,7 +31,111 @@ export default [{
     albumCover: '/assets/images/album_covers/Album3.png',
     slug: 'the-big-hunt',
     songs: [
-      { title: 'Blue', duration: '161.71', playCount: 0, audioSrc: '/assets/music/blue.mp3' },
+      { title: 'Blue', duration: '161.71', playCount: 0, audioSrc: '/assets/music/song-1.mp3' },
+      { title: 'Green', duration: '103.96', playCount: 0, audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', playCount: 0, audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', playCount: 0, audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', playCount: 0, audioSrc: '/assets/music/magenta.mp3' }
+    ]
+}, {
+    title: 'Eat Me',
+    artist: 'ARXV',
+    releaseInfo: '2018 Society Records',
+    albumCover: '/assets/images/album_covers/Album4.png',
+    slug: 'eat-me',
+    songs: [
+      { title: 'Blue', duration: '161.71', playCount: 0, audioSrc: '/assets/music/song-1.mp3' },
+      { title: 'Green', duration: '103.96', playCount: 0, audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', playCount: 0, audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', playCount: 0, audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', playCount: 0, audioSrc: '/assets/music/magenta.mp3' }
+    ]
+}, {
+    title: 'Hot Sand',
+    artist: 'ARXV',
+    releaseInfo: '2018 Society Records',
+    albumCover: '/assets/images/album_covers/Album5.png',
+    slug: 'hot-sand',
+    songs: [
+      { title: 'Blue', duration: '161.71', playCount: 0, audioSrc: '/assets/music/song-1.mp3' },
+      { title: 'Green', duration: '103.96', playCount: 0, audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', playCount: 0, audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', playCount: 0, audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', playCount: 0, audioSrc: '/assets/music/magenta.mp3' }
+    ]
+}, {
+    title: 'Do it',
+    artist: 'ARXV',
+    releaseInfo: '2018 Society Records',
+    albumCover: '/assets/images/album_covers/Album6.png',
+    slug: 'do-it',
+    songs: [
+      { title: 'Blue', duration: '161.71', playCount: 0, audioSrc: '/assets/music/song-1.mp3' },
+      { title: 'Green', duration: '103.96', playCount: 0, audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', playCount: 0, audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', playCount: 0, audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', playCount: 0, audioSrc: '/assets/music/magenta.mp3' }
+    ]
+}, {
+    title: 'Here We Are',
+    artist: 'ARXV',
+    releaseInfo: '2018 Society Records',
+    albumCover: '/assets/images/album_covers/Album7.png',
+    slug: 'here-we-are',
+    songs: [
+      { title: 'Blue', duration: '161.71', playCount: 0, audioSrc: '/assets/music/song-1.mp3' },
+      { title: 'Green', duration: '103.96', playCount: 0, audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', playCount: 0, audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', playCount: 0, audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', playCount: 0, audioSrc: '/assets/music/magenta.mp3' }
+    ]
+}, {
+    title: 'Heavy Metal',
+    artist: 'ARXV',
+    releaseInfo: '2018 Society Records',
+    albumCover: '/assets/images/album_covers/Album8.png',
+    slug: 'heavy-metal',
+    songs: [
+      { title: 'Blue', duration: '161.71', playCount: 0, audioSrc: '/assets/music/song-1.mp3' },
+      { title: 'Green', duration: '103.96', playCount: 0, audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', playCount: 0, audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', playCount: 0, audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', playCount: 0, audioSrc: '/assets/music/magenta.mp3' }
+    ]
+}, {
+    title: 'Within The Walls',
+    artist: 'ARXV',
+    releaseInfo: '2018 Society Records',
+    albumCover: '/assets/images/album_covers/Album9.png',
+    slug: 'within-the-walls',
+    songs: [
+      { title: 'Blue', duration: '161.71', playCount: 0, audioSrc: '/assets/music/song-1.mp3' },
+      { title: 'Green', duration: '103.96', playCount: 0, audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', playCount: 0, audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', playCount: 0, audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', playCount: 0, audioSrc: '/assets/music/magenta.mp3' }
+    ]
+}, {
+    title: 'Debut',
+    artist: 'ARXV',
+    releaseInfo: '2018 Society Records',
+    albumCover: '/assets/images/album_covers/Album10.png',
+    slug: 'debut',
+    songs: [
+      { title: 'Blue', duration: '161.71', playCount: 0, audioSrc: '/assets/music/song-1.mp3' },
+      { title: 'Green', duration: '103.96', playCount: 0, audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', playCount: 0, audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', playCount: 0, audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', playCount: 0, audioSrc: '/assets/music/magenta.mp3' }
+    ]
+}, {
+    title: 'Lame',
+    artist: 'ARXV',
+    releaseInfo: '2018 Society Records',
+    albumCover: '/assets/images/album_covers/Album11.png',
+    slug: 'lame',
+    songs: [
+      { title: 'Blue', duration: '161.71', playCount: 0, audioSrc: '/assets/music/song-1.mp3' },
       { title: 'Green', duration: '103.96', playCount: 0, audioSrc: '/assets/music/green.mp3' },
       { title: 'Red', duration: '268.45', playCount: 0, audioSrc: '/assets/music/red.mp3' },
       { title: 'Pink', duration: '153.14', playCount: 0, audioSrc: '/assets/music/pink.mp3' },

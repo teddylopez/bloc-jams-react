118e6ad61cd26eeb6b3cb09deb3dead49b62619d
teddylopez <tedmlopez@gmail.com>
2018-03-02 21:46:11 -0500

    Styling landing page.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,22 +9,24 @@ class Landing extends Component {
         <header className="hero-content">
           <h1 className="hero-title">Turn up the Ted!</h1>
         </header>
-        <section className="selling-points container clearfix">
+        <section className="selling-points container">
           <div className="arrow"><div className='left'></div><div className='right'></div></div>
-          <div className="point column third">
-            <span className="ion-music-note"></span>
-            <h5 className="point-title">Only Ted</h5>
-            <p className="point-description">Why listen to anything else?</p>
-          </div>
-          <div className="point column third">
-            <span className="ion-radio-waves"></span>
-            <h5 className="point-title">Unlimited streaming, ad-free</h5>
-            <p className="point-description">Tune out the real world</p>
-          </div>
-          <div className="point column third">
-            <span className="ion-iphone"></span>
-            <h5 className="point-title">Mobile enabled</h5>
-            <p className="point-description">Text and drive: 240-271-7135</p>
+          <div className="row">
+            <div className="point col col-xs-12">
+              <span className="ion-music-note"></span>
+              <h5 className="point-title">Only Ted</h5>
+              <p className="point-description">Why listen to anything else?</p>
+            </div>
+            <div className="point col col-xs-12">
+              <span className="ion-radio-waves"></span>
+              <h5 className="point-title">Unlimited streaming, ad-free</h5>
+              <p className="point-description">Tune out the real world</p>
+            </div>
+            <div className="point col col-xs-12">
+              <span className="ion-iphone"></span>
+              <h5 className="point-title">Mobile enabled</h5>
+              <p className="point-description">Text and drive: 240-271-7135</p>
+            </div>
           </div>
         </section>
         <script src="scripts/landing.js"></script>
